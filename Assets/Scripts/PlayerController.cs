932f67697a5923cc19a6d9f6ece29a76a9f66070
Adrian De Andrade <redfoxhint4@hotmail.com>
2018-11-27 12:20:11 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
+        Vector3 targetPos = new Vector3(Mathf.Clamp(xPos, -4.6f, 4.6f), -4.59f, 0f);
+        transform.position = targetPos;
+    }
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float amountToSpawn;
+    [SerializeField] private Vector3 initalSpawnPosition;
+
+    [SerializeField] private GameObject enemyPrefab;
+
+    private Vector3 spawnPosition;
+
+    private void Start()
+    {
+        spawnPosition = initalSpawnPosition;
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            spawnPosition = new Vector3(spawnPosition.x + 1, spawnPosition.y, 0f);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+}
